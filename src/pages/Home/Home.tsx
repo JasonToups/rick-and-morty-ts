c652adc19e0218e72249bbcca05a6e831e356282
jason-toups <me@jasontoups.com>
2020-10-23 15:47:12 -0700

    initial refactor

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import './Home.scss';
-import HomeHeader from '../../components/HomeHeader/HomeHeader';
+import '../../components/HomeHeader/HomeHeader';
+import '../../components/Search/Search';
+import logo from '../../assets/logo-RickAndMorty.png';
+import characteropedia from '../../assets/Characteropedia.png';
+
 import RenderedCards from '../../components/RenderedCards/RenderedCards';
 
 const Home = () => {
   const { useState, useEffect } = React;
   const [results, setResults] = useState(null);
   const [renderCards, setRenderCards] = useState(null);
+
   useEffect(() => {
     getCharacters();
   }, []);
@@ -27,7 +32,33 @@ const Home = () => {
 
   return (
     <section>
-      <HomeHeader />
+      <section className='home-header'>
+        <div className='logos'>
+          <img
+            className='home-header--image'
+            src={logo}
+            alt='rick and morty logo'
+          />
+          <img
+            className='home-header--image'
+            src={characteropedia}
+            alt='characteropedia'
+          />
+        </div>
+        <div className='search-form'></div>
+        <div className='search-form'>
+          <form>
+            <input
+              className='search-field'
+              type='text'
+              placeholder='Search for a character'></input>
+            <input
+              className='submit-button'
+              type='submit'
+              value='submit'></input>
+          </form>
+        </div>
+      </section>
       <RenderedCards />
     </section>
   );
