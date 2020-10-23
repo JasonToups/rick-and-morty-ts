d18363d93f3cd3fbf474a19d3e9c233cb09a6af4
jason-toups <me@jasontoups.com>
2020-10-22 23:44:08 -0700

    Updates

---
+++ precache-manifest.d0c3e981ffe78df92cbfdce2084c5246.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9132e1871c21ef5a1abae88dc7f6539f",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "5451dde9b4afeecd26b3",
+    "url": "/rick-and-morty/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2bda73f8fa7cc312c3c",
+    "url": "/rick-and-morty/static/js/2.cd3df659.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/rick-and-morty/static/js/2.cd3df659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5451dde9b4afeecd26b3",
+    "url": "/rick-and-morty/static/js/main.5514219e.chunk.js"
+  },
+  {
+    "revision": "8ec831075791f93706ea",
+    "url": "/rick-and-morty/static/js/runtime-main.2b22dfe8.js"
+  },
+  {
+    "revision": "bfe774d05328cf9c509739def723adbd",
+    "url": "/rick-and-morty/static/media/Characteropedia.bfe774d0.svg"
+  },
+  {
+    "revision": "96a9d4f06f4d727deeda109bde00d386",
+    "url": "/rick-and-morty/static/media/logo-RickAndMorty.96a9d4f0.png"
+  }
+]);
